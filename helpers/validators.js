d03acdcca41114d33db6816498f2110e551d4ff9
jalvarado-2021353 <jalvarado-2021353@kinal.edu.gt>
2025-03-02 21:30:22 -0600

    Se agregan validaciones para company

--- helpers/validators.js
+++ helpers/validators.js
@@ -7,3 +7,25 @@ export const loginValidator = [
     body('password', 'Password cannot be empty').notEmpty().isStrongPassword().withMessage('The password must be strong').isLength({min: 8}),
     validateErrors
 ]
+
+export const addCompanyValidator = [
+    body("name", "Company name cannot be empty").notEmpty().isLength({ max: 25 }).withMessage("Name cannot exceed 25 characters"),
+    body("description", "Description is required").notEmpty().isLength({ max: 100 }).withMessage("Description cannot exceed 100 characters"),
+    body("category", "Category is required").notEmpty().isLength({ max: 25 }).withMessage("Category cannot exceed 25 characters"),
+    body("impactLevel", "Impact Level is required").notEmpty().isIn(["Alto", "Medio", "Bajo"]).withMessage("Impact Level must be Alto, Medio, or Bajo"),
+    body("trajectoryYears", "Years of experience is required and must be a number").notEmpty().isInt({ min: 0 }).withMessage("Years must be a positive number"),
+    validateErrors
+]
+
+export const updateCompanyValidator = [
+    body("description").optional().isLength({ max: 100 }).withMessage("Description cannot exceed 100 characters"),
+    body("impactLevel").optional().isIn(["Alto", "Medio", "Bajo"]).withMessage("Impact Level must be Alto, Medio, or Bajo"),
+    validateErrors
+];
+
+export const filterCompanyValidator = [
+    body("category").optional().isLength({ max: 25 }).withMessage("Category cannot exceed 25 characters"),
+    body("impactLevel").optional().isIn(["Alto", "Medio", "Bajo"]).withMessage("Impact Level must be Alto, Medio, or Bajo"),
+    body("orderBy").optional().isIn(["A-Z", "Z-A", "trajectory"]).withMessage("OrderBy must be A-Z, Z-A, or trajectory"),
+    validateErrors
+]
--- src/company/company.routes.js
+++ src/company/company.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { save, getAll, update, filterCompanies  } from "./company.controller.js";
 import { validateJwt } from "../../middlewares/validate.jwt.js";
 import { validateAdminRole } from "../../middlewares/validate.role.js"; 
+import { addCompanyValidator, updateCompanyValidator, filterCompanyValidator } from "../../helpers/validators.js";
+
 
 const api = Router();
-api.post("/save", [validateJwt, validateAdminRole], save);
-api.post("/filter", [validateJwt], filterCompanies);
+api.post("/save", [validateJwt, validateAdminRole, addCompanyValidator], save);
+api.post("/filter", [validateJwt, filterCompanyValidator], filterCompanies);
 api.get("/", [validateJwt], getAll);
-api.put("/update/:id", [validateJwt, validateAdminRole], update);
+api.put("/update/:id", [validateJwt, validateAdminRole, updateCompanyValidator], update);
 
 export default api;
